b506f7ffece3eb2e645820213a3a9a8ee1057ff0
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-05-18 16:11:13 -0700

    Multi arch support

--- src/ytdl.ts
+++ src/ytdl.ts
@@ -1,10 +1,16 @@
 import path from 'path'
 import { execFile } from 'child_process'
+import os from 'os'
+
+const arch = os.arch()
 
 export default ( args?: string[] ): Promise<string> => new Promise(
   ( resolve, reject ) => {
     execFile(
-      path.join(__dirname, '../bin/youtube-dl.exe'),
+      path.join(
+        __dirname, 
+        /x32|x64/.test(arch) ? '../bin/youtube-dl.exe' : '../bin/youtube-dl'
+      ),
       args,
       (error, stdout, stderr) => {
         error || stderr 
