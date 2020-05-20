f862d635930a19553007a874a30c4b0238405be1
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-05-20 11:51:52 -0700

    Added GitHub link

--- src/commands/utils/help.ts
+++ src/commands/utils/help.ts
@@ -18,7 +18,9 @@ const ping: Command = {
 
     await props.quickEmbed(
       null, 
-      `${commandList}\n\n[Invite Cherry](https://discord.com/api/oauth2/authorize?client_id=443491966576230400&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fdiscord%2Fauth&scope=bot)`,
+      commandList +
+      `\n\n[Invite Cherry](https://discord.com/api/oauth2/authorize?client_id=443491966576230400&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fdiscord%2Fauth&scope=bot)` +
+      `\n[Fork on GitHub](https://github.com/Andr3wRiv3rs/Cherry)`,
     )
   },
 }
